2126e504ceb81cded2f766ba50e8f5aa594ad4d9
SuRu <33urus@gmail.com>
2022-05-08 20:43:01 +0300

    added mantine appshell

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,38 @@
-const App = () =>
-  <div>
-      Hello
-  </div>;
+import { useState } from "react";
+import { AppShell, ColorScheme, ColorSchemeProvider, MantineProvider } from "@mantine/core";
+import AppHeader from "./components/AppHeader";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Config from "./pages/Config";
+
+const App = () => {
+  const [ colorScheme, setColorScheme ] = useState<ColorScheme>("light");
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+  const appTheme = {
+    colorScheme: colorScheme
+  };
+
+  return (
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider theme={appTheme} withGlobalStyles>
+        <AppShell
+          fixed
+          header={
+            <AppHeader
+              colorScheme={colorScheme}
+              colorSchemeToggleFn={toggleColorScheme}/>
+          }>
+          <Routes>
+            <Route path="/">
+              <Route index element={<Navigate to="home" replace/>}/>
+              <Route path="home" element={<Home/>}/>
+              <Route path="config" element={<Config/>}/>
+            </Route>
+          </Routes>
+        </AppShell>
+      </MantineProvider>
+    </ColorSchemeProvider>
+  );
+};
 export default App;
