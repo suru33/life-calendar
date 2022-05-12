1a322f4dd29aa7279818d24378f2d6fb46f3faf9
SuRu <33urus@gmail.com>
2022-05-13 02:30:45 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
+const container = document.getElementById("root");
+if(container === null) {
+  document.body.innerText = "Failed to run app";
+} else {
+  const root = createRoot(container);
+  root.render(
     <BrowserRouter>
       <App/>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+  );
+}
