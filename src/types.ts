45fa48ee54d9896003df620d8c020e3267395a47
SuRu <33urus@gmail.com>
2022-05-13 02:31:15 +0300

    created types and types utils

---
+++ src/types.ts
@@ -0,0 +1,41 @@
+export type OnlyDate = Date | null;
+
+export type Optional<T> = T | undefined;
+
+export interface LifeEvent {
+  id: string,
+  start: OnlyDate,
+  end: OnlyDate,
+  text: string
+}
+
+export type LifeEvents = LifeEvent[];
+
+export interface LifeEventSer {
+  id: string,
+  start: string,
+  end: string,
+  text: string
+}
+
+export interface LifeBookmark {
+  id: string,
+  date: OnlyDate,
+  text: string
+}
+
+export type LifeBookmarks = LifeBookmark[];
+
+export interface LifeBookmarkSer {
+  id: string,
+  date: string,
+  text: string
+}
+
+export interface LifeEventOverlapError {
+  hidden: boolean,
+  start?: string,
+  end?: string,
+  text?: string
+}
+
