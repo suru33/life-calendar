02c790d6159498a68925219f3cdde2a429acbc3a
SuRu <33urus@gmail.com>
2022-05-14 17:49:55 +0300

    Create app.colors.ts

---
+++ src/app.colors.ts
@@ -0,0 +1,17 @@
+import { DEFAULT_THEME } from "@mantine/core";
+
+export const swatches = [
+  ...DEFAULT_THEME.colors.blue.slice(1, 9),
+  ...DEFAULT_THEME.colors.cyan.slice(1, 9),
+  ...DEFAULT_THEME.colors.grape.slice(1, 9),
+  ...DEFAULT_THEME.colors.green.slice(1, 9),
+  ...DEFAULT_THEME.colors.indigo.slice(1, 9),
+  ...DEFAULT_THEME.colors.lime.slice(1, 9),
+  ...DEFAULT_THEME.colors.orange.slice(1, 9),
+  ...DEFAULT_THEME.colors.pink.slice(1, 9),
+  ...DEFAULT_THEME.colors.red.slice(1, 9),
+  ...DEFAULT_THEME.colors.teal.slice(1, 9),
+  ...DEFAULT_THEME.colors.violet.slice(1, 9),
+  ...DEFAULT_THEME.colors.yellow.slice(1, 9),
+  ...DEFAULT_THEME.colors.gray.slice(1, 9)
+];
